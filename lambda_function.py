50608b4e76a08d955c2f22b09c8fb6ee646a447a
prashant sagar <prashant.sagar73@gmail.com>
2023-11-17 17:19:28 +0530

    update lambda_function

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,8 @@
-import pandas as pd
+import json
 
 def lambda_handler(event, context):
-    print('Hello Prashant sagar')
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello Prashant sagar!')
+    }
